8b6b7964e31710d5194cafd76287f24befc942ac
Leon Zadravec <leon.zadravec@jameda.de>
2021-04-27 20:17:17 +0200

    Change vhost config
    - Change vhost config for security reasons

--- app/public/index.php
+++ app/public/index.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use App\RequestHandler\RequestHandler;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'/../vendor/autoload.php';
 
 $requestHandler =  new RequestHandler();
 $requestHandler->handleRequest();

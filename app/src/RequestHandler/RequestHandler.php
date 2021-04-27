31ddce32ac1053959a396a4cb86ab66949fcc3d4
Leon Zadravec <leon.zadravec@jameda.de>
2021-04-27 21:34:05 +0200

    Setup database connection
    - Add env variables
    - Set up databaseService

--- app/src/RequestHandler/RequestHandler.php
+++ app/src/RequestHandler/RequestHandler.php
@@ -3,8 +3,13 @@ declare(strict_types=1);
 
 namespace App\RequestHandler;
 
+use App\Services\DatabaseService;
+
 class RequestHandler {
 	public function handleRequest() {
+		$databaseService = new DatabaseService();
+		$database = $databaseService->getInstance();
+
 		echo 'YES FINALLY';
 	}
 }
--- app/src/Services/DatabaseService.php
+++ app/src/Services/DatabaseService.php
@@ -5,8 +5,31 @@ declare(strict_types=1);
 namespace App\Services;
 
 
-class DatabaseService {
-	public static function getInstance() {
+use PDO;
+use PDOException;
 
+class DatabaseService {
+	/**
+	 * @var PDO
+	 */
+	private PDO $databaseInstance;
+
+	public function getInstance(): PDO {
+		if (!isset($this->databaseInstance)) {
+			try {
+				$host = getenv('MYSQL_DATABASE_HOST');
+				$database = getenv('MYSQL_DATABASE');
+				$user = getenv('MYSQL_USER');
+				$password = getenv('MYSQL_PASSWORD');
+
+				$this->databaseInstance = new PDO('mysql:host='.$host.';dbname='.$database, $user, $password);
+				return $this->databaseInstance;
+			} catch (PDOException $exception) {
+				print('Error: '.$exception->getMessage());
+				die();
+			}
+		} else {
+			return $this->databaseInstance;
+		}
 	}
 }
